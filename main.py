b14b7d213a80ca5bb0347611673e318dd902c3aa
vuthayou <44387813+vuthayou@users.noreply.github.com>
2026-04-10 19:24:07 +0800

    read the file and count its error status

---
+++ main.py
@@ -0,0 +1,30 @@
+#open file, read only (python default is rt)
+#"with" will close the file automatically
+fileContent = " "
+try:
+    with open("sample.log", "r") as file:
+        fileContent = file.readlines() #returns a list of lines
+except FileNotFoundError:
+    print("File does not exist")
+
+#read file line by line
+#check for types of error and count its error
+# INFO, WARNING, ERROR
+infoCount = 0
+warningCount = 0
+errorCount = 0
+for f in fileContent:
+    if "INFO" in f:
+        infoCount += 1
+    elif "WARNING" in f:
+        warningCount += 1
+    elif "ERROR" in f:
+        errorCount += 1
+    else:
+        print("File is Wrong")
+
+print("Info:", infoCount, "|", "Warning:", warningCount, "|", "Error:", errorCount)
+
+print("ENDL")
+
+
